3a404e78286dc5a66ca890097541456660e9a578
CJarrell <colby.jarrell@icloud.com>
2016-04-04 11:00:12 -0400

    More before after styling

--- js/agency.js
+++ js/agency.js
@@ -26,19 +26,74 @@ $('.navbar-collapse ul li a').click(function() {
 });
 
 //jQuery to reveal before and after images
-$('.be-animate').hover(
+// Before and After Pair 1
+$('.before1').hover(
   function() {
-    $('.be-animate > .reveal-before').slideToggle();
+    $('.before1 > .reveal-before').slideToggle();
   },
   function() {
-    $('.be-animate > .reveal-before').slideToggle();
+    $('.before1 > .reveal-before').slideToggle();
   }
 );
-$('.af-animate').hover(
+$('.after1').hover(
   function() {
-    $('.af-animate > .reveal-after').slideToggle();
+    $('.after1 > .reveal-after').slideToggle();
   },
   function() {
-    $('.af-animate > .reveal-after').slideToggle();
+    $('.after1 > .reveal-after').slideToggle();
+  }
+);
+
+// Before and After Pair 2
+$('.before2').hover(
+  function() {
+    $('.before2 > .reveal-before').slideToggle();
+  },
+  function() {
+    $('.before2 > .reveal-before').slideToggle();
+  }
+);
+$('.after2').hover(
+  function() {
+    $('.after2 > .reveal-after').slideToggle();
+  },
+  function() {
+    $('.after2 > .reveal-after').slideToggle();
+  }
+);
+
+// Before and After Pair 3
+$('.before3').hover(
+  function() {
+    $('.before3 > .reveal-before').slideToggle();
+  },
+  function() {
+    $('.before3 > .reveal-before').slideToggle();
+  }
+);
+$('.after3').hover(
+  function() {
+    $('.after3 > .reveal-after').slideToggle();
+  },
+  function() {
+    $('.after3 > .reveal-after').slideToggle();
+  }
+);
+
+// Before and After Pair 4
+$('.before4').hover(
+  function() {
+    $('.before4 > .reveal-before').slideToggle();
+  },
+  function() {
+    $('.before4 > .reveal-before').slideToggle();
+  }
+);
+$('.after4').hover(
+  function() {
+    $('.after4 > .reveal-after').slideToggle();
+  },
+  function() {
+    $('.after4 > .reveal-after').slideToggle();
   }
 );
